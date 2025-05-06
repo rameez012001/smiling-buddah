18b794817d9693b87821f361c14a39b191ae1dbe
rameez_hameed <rameez012001@gmail.com>
2025-05-06 09:01:21 +0530

    to remote

---
+++ src/main/java/com/smiling/buddah/BuddahApplication.java
@@ -0,0 +1,13 @@
+package com.smiling.buddah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddahApplication.class, args);
+	}
+
+}
